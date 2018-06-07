40251de82b9ad597486a261b2796a083397e8c57
Timo <timo@doersching.net>
2018-06-07 14:55:38 +0200

    add Settings

--- crawl_profile_pi.py
+++ crawl_profile_pi.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from pyvirtualdisplay import Display
 from util.cli_helper import get_all_user_names
 from util.extractor import extract_information
+from .settings import Settings
 
 
 display = Display(visible=0, size=(1024, 768))
@@ -25,11 +26,11 @@ try:
     print('Extracting information from ' + username)
     information, user_commented_list = extract_information(browser, username)
 
-    with open('./profiles/' + username + '.json', 'w') as fp:
+    with open(Settings.profile_location + '/' + username + '.json', 'w') as fp:
       json.dump(information, fp)
 
     print ("Number of users who commented on his/her profile is ", len(user_commented_list),"\n")
-    file = open("./profiles/" + username + "_commenters.txt","w")
+    file = open(Settings.profile_commentors_location + '/' + username + "_commenters.txt","w")
     for line in user_commented_list:
       file.write(line)
       file.write("\n")
