c201a35ce24ab31b802e0d2a1fb0e3e3f77e2129
timo doersching <timo@doersching.net>
2019-01-19 12:27:29 +0100

    crawl follower and write them to json file

---
+++ quickstart_templates/crawl_follower_with_login.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3.5
+"""Goes through all usernames and collects their information"""
+import sys
+from util.settings import Settings
+from util.datasaver import Datasaver
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.firefox.options import Options as Firefox_Options
+
+from util.cli_helper import get_all_user_names
+from util.extractor import extract_information
+from util.account import login
+from util.chromedriver import init_chromedriver
+
+
+chrome_options = Options()
+chromeOptions = webdriver.ChromeOptions()
+prefs = {'profile.managed_default_content_settings.images':2, 'disk-cache-size': 4096}
+chromeOptions.add_experimental_option("prefs", prefs)
+chrome_options.add_argument('--dns-prefetch-disable')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--lang=en-US')
+chrome_options.add_argument('--headless')
+chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en-US'})
+
+capabilities = DesiredCapabilities.CHROME
+
+
+try:
+    browser = init_chromedriver(chrome_options, capabilities)
+except Exception as exc:
+    print(exc)
+    sys.exit()
+
+
+Settings.login_username = 'username'
+Settings.login_password = 'password'
+Settings.scrap_follower = True
+Settings.scrap_posts_likers = False
+Settings.scrap_posts_infos = False
+try:
+    usernames = get_all_user_names()
+    for username in usernames:
+        print('Extracting information from ' + username)
+        information = []
+        user_commented_list = []
+        try:
+            if len(Settings.login_username) != 0:
+                login(browser, Settings.login_username, Settings.login_password)
+            information, user_commented_list = extract_information(browser, username, Settings.limit_amount)
+        except:
+            print("Error with user " + username)
+            sys.exit(1)
+        Datasaver.save_profile_json(username,information)
+
+except KeyboardInterrupt:
+    print('Aborted...')
+
+finally:
+    browser.delete_all_cookies()
+    browser.close()
--- util/extractor.py
+++ util/extractor.py
@@ -1,6 +1,6 @@
 """Methods to extract the data for the given usernames profile"""
 import sys
-from time import sleep
+from time import sleep, time
 from re import findall
 import math
 
@@ -19,29 +19,25 @@ from util.instalogger import InstaLogger
 from util.exceptions import PageNotFound404, NoInstaProfilePageFound
 
 
-def get_user_info(browser):
+def get_user_info(browser, username):
     """Get the basic user info from the profile screen"""
     num_of_posts = 0
-    followers = 0
-    following = 0
+    followers = { 'count' : 0}
+    following = { 'count' : 0}
     prof_img = ""
     bio = ""
     bio_url = ""
-    alias_name = ""
+    alias = ""
     container = browser.find_element_by_class_name('v9tJq')
     isprivate = False
-
     try:
-        infos = container.find_elements_by_class_name('Y8-fY')
-        num_of_posts = extract_exact_info(infos[0])
-        followers = extract_exact_info(infos[1])
-        following = extract_exact_info(infos[2])
+        if container.find_element_by_class_name('Nd_Rl'):
+            isprivate = True
     except:
-        InstaLogger.logger().error("Infos (Follower, Abo, Posts) is empty")
-        infos = ""
+        isprivate = False
 
     try:
-        alias_name = container.find_element_by_class_name('-vDIg').find_element_by_tag_name('h1').text
+        alias = container.find_element_by_class_name('-vDIg').find_element_by_tag_name('h1').text
     except:
         InstaLogger.logger().info("alias is empty")
 
@@ -63,19 +59,60 @@ def get_user_info(browser):
         InstaLogger.logger().info("image is empty")
 
     try:
-        if container.find_element_by_class_name('Nd_Rl'):
-            isprivate = True
-    except:
-        isprivate = False
+        infos = container.find_elements_by_class_name('Y8-fY')
 
-    InstaLogger.logger().info("alias name: " + alias_name)
-    InstaLogger.logger().info("bio: " + bio)
-    InstaLogger.logger().info("url: " + bio_url)
-    InstaLogger.logger().info("Posts: " + str(num_of_posts))
-    InstaLogger.logger().info("Follower: " + str(followers))
-    InstaLogger.logger().info("Following: " + str(following))
-    InstaLogger.logger().info("isPrivate: " + str(isprivate))
-    return alias_name, bio, prof_img, num_of_posts, followers, following, bio_url, isprivate
+        try:
+            num_of_posts = extract_exact_info(infos[0])
+        except:
+            InstaLogger.logger().error("Number of Posts empty")
+
+
+        try:
+            following = { 'count' : extract_exact_info(infos[2])}
+        except:
+            InstaLogger.logger().error("Following is empty")
+
+        try:
+            followers = { 'count' : extract_exact_info(infos[1])}
+
+            try:
+                if Settings.scrap_follower == True:
+                    if isprivate == True:
+                        InstaLogger.logger().info("Cannot get Follower List - private account")
+                    else:
+                        followers['list'] = extract_followers(browser, username)
+            except Exception as exception:
+                # Output unexpected Exceptions.
+                print("Unexpected error:", sys.exc_info()[0])
+                print(exception)
+
+                InstaLogger.logger().error("Cannot get Follower List")
+        except:
+            InstaLogger.logger().error("Follower is empty")
+    except:
+        InstaLogger.logger().error("Infos (Following, Abo, Posts) is empty")
+
+
+    information = {
+        'alias': alias,
+        'username': username,
+        'bio': bio,
+        'prof_img': prof_img,
+        'num_of_posts': num_of_posts,
+        'followers': followers,
+        'following': following,
+        'bio_url': bio_url,
+        'isprivate': isprivate,
+    }
+
+    InstaLogger.logger().info("alias name: " + information['alias'])
+    InstaLogger.logger().info("bio: " + information['bio'])
+    InstaLogger.logger().info("url: " + information['bio_url'])
+    InstaLogger.logger().info("Posts: " + str(information['num_of_posts']))
+    InstaLogger.logger().info("Follower: " + str(information['followers']['count']))
+    InstaLogger.logger().info("Following: " + str(information['following']))
+    InstaLogger.logger().info("isPrivate: " + str(information['isprivate']))
+    return information
 
 
 def extract_exact_info(info):
@@ -90,6 +127,89 @@ def extract_exact_info(info):
         else:
             exact_info = int(exact_info.replace('k', '000').replace('m', '000000'))
     return exact_info
+
+
+def extract_followers(browser, username):
+    InstaLogger.logger().info('Extracting information from ' + username)
+    try:
+        user_link = "https://www.instagram.com/{}".format(username)
+        web_adress_navigator(browser, user_link)
+    except PageNotFound404 as e:
+        raise NoInstaProfilePageFound(e)
+    sleep(5)
+
+    followers = []
+
+    # find number of followers
+    elem = browser.find_element_by_xpath(
+        "//span[@id='react-root']//header[@class='vtbgv ']//ul[@class='k9GMp ']/child::li[2]/a/span")
+    elem.click()
+    sleep(15)
+
+    # remove suggestion list and load 24 list elements after this
+    browser.execute_script("document.getElementsByClassName('isgrP')[0].scrollTo(0,500)")
+    sleep(10)
+
+    elems = browser.find_elements_by_xpath("//body//div[@class='PZuss']//a[@class='FPmhX notranslate _0imsa ']")
+    for i in range(12):
+        val = elems[i].get_attribute('innerHTML')
+        followers.append(val)
+
+    for i in range(12):
+        browser.execute_script("document.getElementsByClassName('PZuss')[0].children[0].remove()")
+
+    isDone = False
+
+    while 1:
+        try:
+
+            start = time()
+            browser.execute_script(
+                "document.getElementsByClassName('isgrP')[0].scrollTo(0,document.getElementsByClassName('isgrP')[0].scrollHeight)")
+
+            while 1:
+                try:
+                    if int(browser.execute_script(
+                            "return document.getElementsByClassName('PZuss')[0].children.length")) == 24:
+                        break
+                except (KeyboardInterrupt, SystemExit):
+                    # f.close()
+                    raise
+                except:
+                    continue
+                if time() - start > 10:
+                    isDone = True
+                    break
+
+            if isDone:
+                break
+
+            elems = browser.find_elements_by_xpath("//body//div[@class='PZuss']//a[@class='FPmhX notranslate _0imsa ']")
+            list_segment = "";
+            for i in range(12):
+                val = elems[i].get_attribute('innerHTML')
+                list_segment += (val + '\n')
+                followers.append(val)
+
+            for i in range(12):
+                browser.execute_script("document.getElementsByClassName('PZuss')[0].children[0].remove()")
+
+            print(time() - start)
+
+        except (KeyboardInterrupt, SystemExit):
+            # f.close()
+            raise
+        except:
+            continue
+
+    list_segment = "";
+    elems = browser.find_elements_by_xpath("//body//div[@class='PZuss']//a[@class='FPmhX notranslate _0imsa ']")
+    for i in range(len(elems)):
+        val = elems[i].get_attribute('innerHTML')
+        list_segment += (val + '\n')
+        followers.append(val)
+
+    return followers
 
 
 def extract_user_posts(browser, num_of_posts_to_do):
@@ -178,7 +298,7 @@ def extract_user_posts(browser, num_of_posts_to_do):
         counter = counter + 1
 
         try:
-            caption, location_url, location_name, location_id, lat, lng, img, tags, likes, commentscount, date, user_commented_list, user_comments, mentions,user_liked_post = extract_post_info(
+            caption, location_url, location_name, location_id, lat, lng, img, tags, likes, commentscount, date, user_commented_list, user_comments, mentions, user_liked_post = extract_post_info(
                 browser, postlink)
 
             location = {
@@ -227,21 +347,18 @@ def extract_information(browser, username, limit_amount):
         raise NoInstaProfilePageFound(e)
 
     num_of_posts_to_do = 999999
-    alias_name = ''
-    bio = ''
-    prof_img = ''
-    num_of_posts = 0
-    followers = 0
-    following = 0
-    bio_url = ''
+
+
     try:
-        alias_name, bio, prof_img, num_of_posts, followers, following, bio_url, isprivate = get_user_info(browser)
+        userinfo = get_user_info(browser, username)
         if limit_amount < 1:
             limit_amount = 999999
-        num_of_posts_to_do = min(limit_amount, num_of_posts)
+        num_of_posts_to_do = min(limit_amount, userinfo['num_of_posts'])
     except Exception as err:
         InstaLogger.logger().error("Couldn't get user profile. - Terminating")
         quit()
+
+
     prev_divs = browser.find_elements_by_class_name('_70iju')
 
     post_infos = []
@@ -252,20 +369,8 @@ def extract_information(browser, username, limit_amount):
         except:
             InstaLogger.logger().error("Couldn't get user posts.")
 
-    information = {
-        'alias': alias_name,
-        'username': username,
-        'bio': bio,
-        'prof_img': prof_img,
-        'num_of_posts': num_of_posts,
-        'followers': followers,
-        'following': following,
-        'bio_url': bio_url,
-        'isprivate': isprivate,
-        'scrapped': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        'posts': post_infos,
-    }
-
+    userinfo['posts'] = post_infos
+    userinfo['scrapped'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     InstaLogger.logger().info("User " + username + " has " + str(len(user_commented_total_list)) + " comments.")
 
     # sorts the list by frequencies, so users who comment the most are at the top
@@ -285,4 +390,4 @@ def extract_information(browser, username, limit_amount):
                 user_commented_list.append(user_commented_total_list[i])
             last = user_commented_total_list[i]
 
-    return information, user_commented_list
+    return userinfo, user_commented_list
