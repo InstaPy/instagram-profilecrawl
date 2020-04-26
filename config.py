8f5326abdb14c17fd007cd49404ed6b237316e9e
asrulsibaoel <asrul@kata.ai>
2020-04-26 14:33:32 +0700

    Added: IG username & Password

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
+from os import getenv
+from dotenv import load_dotenv
 
 ROOT_DIR = abspath(dirname(__file__))
+dotenv_path = join(ROOT_DIR, '.env')
+load_dotenv(dotenv_path)
+IG_USERNAME = getenv("IG_USERNAME", "")
+IG_PASSWORD = getenv("IG_PASSWORD", "")
